d47cfe862efc080560301dc871c5d3a71704a06d
roadrunnerair <machine.x240@gmail.com>
2021-08-20 17:43:17 +0900

    ProtoSpace挙動確認の修正3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
   def update
     @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to user_path(@prototype.user)
+      redirect_to prototype_path(@prototype.id)
     else
       render :edit
     end
